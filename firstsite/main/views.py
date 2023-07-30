c32f9cd67506426bd1435daa826edb8722914ff1
Burlakov Anton <burlakovanton99@yandex.ru>
2023-07-30 21:30:13 +0300

    second set

--- firstsite/main/views.py
+++ firstsite/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('<h4>Проверка работы</h4>')
+
+
+def about(request):
+    return HttpResponse('<h4>страница про нас</h4>')
+
+
+def additionally(request):
+    return HttpResponse('<h4>дополнительно</h4>')
